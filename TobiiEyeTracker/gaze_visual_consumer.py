c14fc14f969e53be2bfdd2b3d53982a339478bf3
Timothy Mwiti <mwititimothyk@gmail.com>
2019-05-28 11:54:42 -0500

    visual plot partly working. axes are flipped

--- TobiiEyeTracker/gaze_visual_consumer.py
+++ TobiiEyeTracker/gaze_visual_consumer.py
@@ -3,26 +3,28 @@ import numpy as np
 import math
 
 # TODO: See if you need to adjust the figure size
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_title('Gaze Color Map')
-hits_array = np.empty((768, 1024))
-
+fig = ""
+ax = []
+hits_array = np.empty((1920, 1080))
+flag = True
+monitor_size = [1920, 1080]
 
 def getOptions(opts, vars):
-    opts['monitor_size'] = [768, 1024]
+    opts['monitor_size'] = [1920, 1080]
 
 def consume_enter(sins, board, opts, vars):
     print("Setting up hits array...")
     global hits_array
     hits_array = np.zeros((math.ceil(opts["monitor_size"][0]/10), math.ceil(opts["monitor_size"][1]/10)))
+    monitor_size[0] = opts["monitor_size"][0]
+    monitor_size[1] = opts["monitor_size"][1]
+    print(hits_array.shape)
     print("Finished setting up hits array...")
 
 def consume(info, sins, board, opts, vars):
     print("Consume called...")
     if sins[0][1] != 0:
         main(sins[0])
-
 
 def consume_flush(sins, board, opts, vars):
     pass
@@ -32,11 +34,26 @@ def main(data):
     global fig
     global ax
     global hits_array
+    global flag
+
+    if flag:
+        global monitor_size
+        plt.ion()
+        fig = plt.figure()
+        plt.xlim(0, monitor_size[0] + 2)
+        plt.ylim(0, monitor_size[1] + 2)
+        ax = fig.add_subplot(111)
+        ax.set_title('Gaze Color Map')
+        plt.draw()
+        flag = False
 
 
     x_coord = int(data[0])
     y_coord = int(data[1])
 
-    hits_array[int(x_coord/10), int(y_coord/10)] += 1
+    print("x_coord: ", x_coord)
+    print("y_coord: ", y_coord)
+
+    hits_array[int(x_coord/10), int(y_coord/10)] += 10
     plt.imshow(hits_array)
-    plt.pause(2)
+    plt.pause(0.000000001)
